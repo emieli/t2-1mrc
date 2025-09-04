96f69af469a4ad10fa3a282583f8dfd8c1465bde
emileli <emil.eliasson@tele2.com>
2025-09-04 10:30:44 +0200

    Rename variables for added clarity

--- solution/main.py
+++ solution/main.py
@@ -1,35 +1,34 @@
 def main():
 
-    avg_temp_by: dict = {}
+    temp_by: dict = {}
 
     with open("measurements.txt", "r") as file:
         for line in file:
-            city_name, city_temp = line.split(";")
-            city_temp = float(city_temp)
+            city, temp = line.split(";")
+            temp = float(temp)
 
-            if city_name not in avg_temp_by:
-                avg_temp_by[city_name] = {
-                    "min": city_temp,
-                    "max": city_temp,
-                    "sum": city_temp,
+            if city not in temp_by:
+                temp_by[city] = {
+                    "min": temp,
+                    "max": temp,
+                    "sum": temp,
                     "counter": 1,
                 }
                 continue
 
-            if city_temp > avg_temp_by[city_name]["max"]:
-                avg_temp_by[city_name]["max"] = city_temp
+            if temp > temp_by[city]["max"]:
+                temp_by[city]["max"] = temp
 
-            if city_temp < avg_temp_by[city_name]["min"]:
-                avg_temp_by[city_name]["min"] = city_temp
+            if temp < temp_by[city]["min"]:
+                temp_by[city]["min"] = temp
 
-            avg_temp_by[city_name]["sum"] += city_temp
-            avg_temp_by[city_name]["counter"] += 1
+            temp_by[city]["sum"] += temp
+            temp_by[city]["counter"] += 1
 
-    for city_name in sorted(avg_temp_by.keys()):
-        city = avg_temp_by[city_name]
-        min = city["min"]
-        max = city["max"]
-        mean = city["sum"] / city["counter"]
-        print(f"{city_name};{min};{mean:.1f};{max}")
+    for city, temp in temp_by.items():
+        min = temp["min"]
+        max = temp["max"]
+        avg = temp["sum"] / temp["counter"]
+        print(f"{city};{min};{avg:.1f};{max}")
 
 main()
